955430a8e1f155345d97f7b94565477c596684d0
24HP5665 24HP5665 <24HP5665@admins-iMac.local>
2024-11-22 17:44:34 +0800

    update commit

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 import express from "express";
+import * as fs from "fs";
+
 
 const PORT = 3333;
 const app = express();
 app.use(express.json());
 
-const todos = [
-    {
-        id: 1,
-        title: "Wake up",
-        checked: false,
-    },
-];
+let todos = [];
 
 app.get("/todos", (req, res) => {
-    res.send(todos);
+  const data = fs.readFile("./data.json", "utf-8");
+  todos = JSON.parse(data);
+  todos = res.send(todos);
 });
 
 app.post("/todos", (req, res) => {
@@ -24,8 +22,41 @@ app.post("/todos", (req, res) => {
     title: title,
     checked: false,
   };
+  console.log(newTodo);
   todos.push(newTodo);
   return res.send(newTodo);
+});
+
+// http://localhost:3333/todos/1
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.prams.id;
+  if(!id) return res.status(404).send({ message: "Id not found!" });
+  const todo = todos.find((item) => item.id === Number(id));
+  if(!todo) return res.status(404).send({ message: "Todo not found!" });
+  return res.send(todo);
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  if(!id) return res.statusMessage(400).send({ message: "Id not found"});
+  // TODO delete functions
+});
+
+app.put("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  if (!id) return res.statusMessage(400).send({ message: "Id not found" });
+  // TODO update functions
+});
+
+app.post("/signup", (req, res) => {
+  const {email, password} = req.body;
+  const users = JSON.parse(fs.readFileSync("./user.json", "utf-8"));
+  const existingUser = users.find((user) => user.email === email);
+  if(existingUser)
+    return res.status(400).send({message: "Email already registered!"});
+  fs.writeFileSync('./user.json',JSON.stringify([...users, newUser]));
+  return res.status(201).send(newUser);
 });
 
 app.listen(PORT, () => {
